25ec7d5e64d68f812a36d830efec3163101fab00
dcouch440 <dcouch440@gmail.com>
2021-08-17 22:55:11 -0700

    change to nil

--- spec/factories/visits.rb
+++ spec/factories/visits.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :visit do
     check_in { "2021-08-17 16:03:19" }
-    check_out { "2021-08-17 16:03:19" }
-    member { build(:employee) }
+    check_out { nil }
+    member { build(:member) }
   end
 end
