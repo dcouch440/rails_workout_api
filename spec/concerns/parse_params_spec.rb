6d88212b729263d50be02cb7a550741f9eb3d242
dcouch440 <dcouch440@gmail.com>
2021-08-17 22:54:54 -0700

    add parse methods

--- spec/concerns/parse_params_spec.rb
+++ spec/concerns/parse_params_spec.rb
@@ -12,30 +12,49 @@ describe 'ParseParams' do
   }}
 
   let!(:gym_params) {{
-    :name => 'Bob',
+    :name => 'Bobs Gym',
     :established => '1/11/1111'
   }}
 
-  describe '#parse_location' do
+  let!(:member_params){{
+    :name => 'Bob',
+    :membership_active => false
+  }}
 
-    context 'when there are additional props' do
+  let!(:location_gym_params){{
+    **location_params,
+    **gym_params,
+  }}
 
-      it 'return the correct arguments for Location' do
-        params_with_location = { **location_params, **gym_params }
-        expect(ParseParams::parse_gym params_with_location).to eql location_params
+  let!(:location_member_params){{
+    **location_params,
+    **member_params
+  }}
+
+
+  context 'when there are additional props' do
+
+    describe '#parse_location' do
+
+      it 'returns the correct arguments for Location' do
+        expect(ParseParams::parse_location location_gym_params).to eql location_params
+        expect(ParseParams::parse_location location_member_params).to eql location_params
       end
 
     end
 
-  end
+    describe '#parse_gym' do
 
-  describe '#parse_gym' do
+      it 'returns the correct arguments for Gym' do
+        expect(ParseParams::parse_gym location_gym_params).to eql gym_params
+      end
 
-    context 'when there are additional props' do
+    end
 
-      it 'return the correct arguments for Gym' do
-        params_with_location = { **location_params, **gym_params }
-        expect(ParseParams::parse_gym params_with_location).to eql gym_params
+    describe '#parse_member' do
+
+      it 'returns the correct params for Member' do
+        expect(ParseParams::parse_member location_member_params).to eql member_params
       end
 
     end
