88fc856a0695a8ad013ee221fc4e02a31262f7f2
dcouch440 <dcouch440@gmail.com>
2021-08-17 22:00:03 -0700

    fix typo

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,6 +1,7 @@
 class Location < ApplicationRecord
+
   has_one :gym
-  has_one :user
+  has_one :member
   has_one :employee
 
   before_save :add_slug
@@ -17,4 +18,5 @@ class Location < ApplicationRecord
   def add_slug
     self.slug = city.parameterize
   end
+
 end
