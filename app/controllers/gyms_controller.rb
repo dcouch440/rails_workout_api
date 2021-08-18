81789edb7369d363c70d2ac043b121c038ee9547
dcouch440 <dcouch440@gmail.com>
2021-08-18 00:31:11 -0700

    add controller route methods show update destroy

--- app/controllers/gyms_controller.rb
+++ app/controllers/gyms_controller.rb
@@ -4,13 +4,33 @@ class GymsController < ApplicationController
     json_response(Gym.all)
   end
 
+  def show
+    json_response(Gym.find(params[:id]))
+  end
+
+  def update
+    json_response(Gym.find_and_update(update_params))
+  end
+
+  def destroy
+    Gym.find(params[:id]).destroy!
+    json_response({message: 'deleted'}, :no_content)
+  end
+
   def create
-    json_response(Gym.create_gym_with_location(gym_params), :created)
+    json_response(Gym.create_gym_with_location(gym_creation_params), :created)
   end
 
   private
 
-  def gym_params
+  def update_params
+    params.permit %i[
+      name
+      established
+    ]
+  end
+
+  def gym_creation_params
     params.permit %i[
       street_address
       state_province
