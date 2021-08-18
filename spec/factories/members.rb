0fac1bca454c8b9ec0463206f8691904fd7deb6a
dcouch440 <dcouch440@gmail.com>
2021-08-18 12:24:17 -0700

    change from bool to string

--- spec/factories/members.rb
+++ spec/factories/members.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :member do
     name { "MyString" }
-    membership_active { true }
+    membership_active { 'active' }
     gym { build(:gym) }
     location { build(:location) }
   end
