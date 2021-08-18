2fd20c9ce8f1c0eb01e422611068a993d9df63e6
dcouch440 <dcouch440@gmail.com>
2021-08-17 18:53:29 -0700

    add create_gym_method

--- app/models/gym.rb
+++ app/models/gym.rb
@@ -2,4 +2,19 @@ class Gym < ApplicationRecord
   belongs_to :location
   has_many :members
   has_many :employees
+
+  def self.create_gym_with_location params
+    location = {
+      street_address: params[:street_address],
+      state_province: params[:state_province],
+      city: params[:city],
+      postal_code: params[:postal_code]
+    }
+    gym = {
+      name: params[:name],
+      established: params[:established]
+    }
+    Location.create!(location).create_gym(gym)
+  end
+
 end
