69600719b8c7e7db6e64f8ceff89a9804bf96ab4
dcouch440 <dcouch440@gmail.com>
2021-08-18 14:13:52 -0700

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,15 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :gyms, only: %i[ create index show update destroy ] do
     resource :members, only: %i[ create update destroy show ]
     resource :employees, only: %i[ create update destroy show ]
   end
 
-  resources :employees, only: %i[ create update destroy show ] do
+  resources :employees, only: %i[ create index show update ] do
     resource :shifts, only: %i[ create update ]
   end
 
-  resources :members, only: %i[ create index update destroy show ] do
+  resources :members, only: %i[ create index show update ] do
     resource :visits, only: %i[ create update ]
   end
 
