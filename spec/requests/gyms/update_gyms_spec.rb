3cb30e4da6040f488a31c43483d67de97370da6c
dcouch440 <dcouch440@gmail.com>
2021-08-18 12:24:08 -0700

    add update spec - PASS

---
+++ spec/requests/gyms/update_gyms_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+describe 'Update Gym', :type => :request do
+
+  let!(:gym) { FactoryBot.create(:gym) }
+  let!(:gym_id) { gym.id }
+  let!(:patch_params) {{ name: "Terry's Gym", established: '1111-11-01' }}
+
+  describe '#update' do
+
+    before do
+      patch gym_path(gym_id), params: patch_params
+      @response_name,
+      @response_established,
+      @response_id = JSON
+                       .parse(response.body)
+                       .values_at 'name',
+                                  'established',
+                                  'id'
+
+    end
+
+    it 'update a gym' do
+      expect(@response_name).to eql patch_params[:name]
+      expect(@response_established).to eql patch_params[:established]
+      expect(@response_id).to eql gym_id
+    end
+
+  end
+
+end
