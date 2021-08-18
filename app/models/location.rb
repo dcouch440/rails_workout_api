25bb90cd8d6b9035bc6eb0e5d3d4c2985e28a4fe
dcouch440 <dcouch440@gmail.com>
2021-08-17 18:54:37 -0700

    add slug life cycle

--- app/models/location.rb
+++ app/models/location.rb
@@ -2,4 +2,12 @@ class Location < ApplicationRecord
   has_one :gym
   has_one :user
   has_one :employee
+
+  before_save :add_slug
+
+  private
+
+  def add_slug
+    self.slug = city.parameterize
+  end
 end
