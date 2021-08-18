376d295d2f63de699fe0ef5d342f9a805ca3cd24
dcouch440 <dcouch440@gmail.com>
2021-08-17 22:54:24 -0700

    add update validation

--- app/models/shift.rb
+++ app/models/shift.rb
@@ -2,4 +2,16 @@ class Shift < ApplicationRecord
 
   belongs_to :employee
 
+
+  before_update :check_if_check_out_is_present
+  validates :check_in, presence: true
+
+  private
+
+  def check_if_check_out_is_present
+    if check_out.nil?
+      errors.add(:check_out, 'check_out must be present in order to update')
+    end
+  end
+
 end
