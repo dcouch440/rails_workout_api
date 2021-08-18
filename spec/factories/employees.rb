149fd80b1cb7e547e67d00d2d112476efd9f4a97
dcouch440 <dcouch440@gmail.com>
2021-08-18 14:13:44 -0700

    change factory bot

--- spec/factories/employees.rb
+++ spec/factories/employees.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :employee do
-    name { "MyString" }
+    name { "Bob Ross" }
     hired { "2021-08-17" }
-    employment_active { true }
+    employment_active { 'active' }
     gym { build(:gym) }
     location { build(:location) }
   end
