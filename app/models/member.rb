c697a94821c05e9296c9108d14570ce566400ca7
dcouch440 <dcouch440@gmail.com>
2021-08-17 22:00:12 -0700

    add validations

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,22 +1,21 @@
 class Member < ApplicationRecord
 
   belongs_to :gym
+  belongs_to :location
+
   has_many :visits
+
+  validates :membership_active, inclusion: [true, false]
+  validates :membership_active, exclusion: [nil]
 
   validates *%i[
     name
     signed_up
-    membership
+    membership_active
   ], presence: true
 
   before_save %i[
     downcase_membership
   ]
-
-  private
-
-  def downcase_membership
-    self.membership = membership.downcase
-  end
 
 end
