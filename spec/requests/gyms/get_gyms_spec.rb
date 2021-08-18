2463523ac13e8e9deec1716b0c0bf3fc1a60bdc6
dcouch440 <dcouch440@gmail.com>
2021-08-17 18:54:05 -0700

    create tests for get

---
+++ spec/requests/gyms/get_gyms_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+describe 'GET Gym', :type => :request do
+
+  let!(:gym) { FactoryBot.create(:gym) }
+
+  describe '#index' do
+
+    before do
+      get gyms_path
+
+      @response_name,
+      @response_established,
+      @response_location_id = JSON
+                               .parse(response.body)
+                               .first()
+                               .values_at 'name',
+                                          'established',
+                                          'location_id'
+
+      @gym_established_string = gym.established.to_s()
+      @gym_location_id = gym.location.id
+      @gym_name = gym.name
+
+    end
+
+    it 'should return a gym' do
+      expect(@response_name).to eql @gym_name
+      expect(@response_established).to eql @gym_established_string
+      expect(@response_location_id).to eql @gym_location_id
+    end
+
+  end
+
+end
