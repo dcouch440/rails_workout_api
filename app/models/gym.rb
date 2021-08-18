77d7783793b0898d4d6a63e482856cce0e6a6bfa
dcouch440 <dcouch440@gmail.com>
2021-08-18 14:12:48 -0700

    add gym association

--- app/models/gym.rb
+++ app/models/gym.rb
@@ -11,11 +11,11 @@ class Gym < ApplicationRecord
     established
   ], presence: true
 
-
   def self.create_gym_with_location params
     location = ParseParams::parse_location params
     gym = ParseParams::parse_gym params
-    Location.create!(location).create_gym(gym)
+    location = Location.create!(location)
+    location.create_gym!(gym)
   end
 
 end
