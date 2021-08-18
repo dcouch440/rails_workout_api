a83fbd2cd171f217535420e3a1aa3c29a37c16f3
dcouch440 <dcouch440@gmail.com>
2021-08-17 22:55:03 -0700

    remove column

--- spec/factories/members.rb
+++ spec/factories/members.rb
@@ -1,7 +1,6 @@
 FactoryBot.define do
   factory :member do
     name { "MyString" }
-    signed_up { "2021-08-17" }
     membership_active { true }
     gym { build(:gym) }
     location { build(:location) }
