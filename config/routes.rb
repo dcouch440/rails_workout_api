109e97c13f9e97924300d9073b791dce35b56601
dcouch440 <dcouch440@gmail.com>
2021-08-18 00:30:41 -0700

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :gyms
+  resources :gyms, only: %i[ create index show destroy ] do
+    resource :members, only: %i[ create update destroy show ]
+    resource :employees, only: %i[ create update destroy show ]
+  end
+
+  resources :employees, only: %i[ create update destroy show ] do
+    resource :shifts, only: %i[ create update ]
+  end
+
+  resources :members, only: %i[ create update destroy show ] do
+    resource :visits, only: %i[ create update ]
+  end
+
 end
