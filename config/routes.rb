12ee9bbf5e13a6a70c6fe973b298f39b328f9525
dcouch440 <dcouch440@gmail.com>
2021-08-18 14:32:51 -0700

    namespace routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,18 @@
 Rails.application.routes.draw do
 
-  resources :gyms, only: %i[ create index show update destroy ] do
-    resource :members, only: %i[ create update destroy show ]
-    resource :employees, only: %i[ create update destroy show ]
-  end
+  namespace :v1 do
+    resources :gyms, only: %i[ create index show update destroy ] do
+      resource :members, only: %i[ create update destroy show ]
+      resource :employees, only: %i[ create update destroy show ]
+    end
 
-  resources :employees, only: %i[ create index show update ] do
-    resource :shifts, only: %i[ create update ]
-  end
+    resources :employees, only: %i[ create index show update ] do
+      resource :shifts, only: %i[ create update ]
+    end
 
-  resources :members, only: %i[ create index show update ] do
-    resource :visits, only: %i[ create update ]
+    resources :members, only: %i[ create index show update ] do
+      resource :visits, only: %i[ create update ]
+    end
   end
 
 end
