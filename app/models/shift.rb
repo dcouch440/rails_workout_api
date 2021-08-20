89c7aec383881b33b017cf383a4a969048993956
dcouch440 <dcouch440@gmail.com>
2021-08-19 18:19:10 -0700

    add clock in logic

--- app/models/shift.rb
+++ app/models/shift.rb
@@ -1,15 +1,26 @@
 class Shift < ApplicationRecord
 
+  include ParseParams
+
   belongs_to :employee
 
-  before_update :check_if_check_out_is_present
-  validates :check_in, presence: true
+  validates :check_in,
+            presence: true
 
-  private
+  validates :check_out,
+            presence: true,
+            on: :update
 
-  def check_if_check_out_is_present
-    if check_out.nil?
-      errors.add(:check_out, 'check_out must be present in order to update')
+  def self.add_visitation_status params
+    time_update = params[:time]
+    employee = Employee.find(params[:employee_id])
+    shift = employee.shifts.last
+    if shift == nil || shift['check_out'] != nil
+      created_shift = employee.shifts.create!({ check_in: time_update })
+      [ created_shift, :created ]
+    else
+      shift.update!({ check_out: time_update })
+      [ shift, :ok ]
     end
   end
 
--- app/models/visit.rb
+++ app/models/visit.rb
@@ -1,5 +1,27 @@
 class Visit < ApplicationRecord
 
+  include ParseParams
+
   belongs_to :member
+
+  validates :check_in,
+            presence: true
+
+  validates :check_out,
+            presence: true,
+            on: :update
+
+  def self.add_visitation_status params
+    time_update = params[:time]
+    member = Member.find(params[:member_id])
+    visit = member.visits.last
+    if visit == nil || visit['check_out'] != nil
+      created_visit = member.visits.create!({ check_in: time_update })
+      [ created_visit, :created]
+    else
+      visit.update!({ check_out: time_update })
+      [ visit, :ok ]
+    end
+  end
 
 end
