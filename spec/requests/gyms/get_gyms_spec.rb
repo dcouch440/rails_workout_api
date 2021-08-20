daa2041eb8a60b0af78909e23f64bf0f0da4db5f
dcouch440 <dcouch440@gmail.com>
2021-08-19 18:25:37 -0700

    add status code

--- spec/requests/gyms/get_gyms_spec.rb
+++ spec/requests/gyms/get_gyms_spec.rb
@@ -7,7 +7,10 @@ describe 'GET Gym', :type => :request do
   describe '#index' do
 
     before do
+
       get v1_gyms_path
+
+      @response = response
 
       @response_name,
       @response_established,
@@ -28,6 +31,10 @@ describe 'GET Gym', :type => :request do
       expect(@response_name).to eql @gym_name
       expect(@response_established).to eql @gym_established_string
       expect(@response_location_id).to eql @gym_location_id
+    end
+
+    it 'should return with a 200 response status' do
+      expect(@response).to have_http_status :ok
     end
 
   end
