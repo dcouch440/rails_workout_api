d853ebc582e954d7bf0a26a7c465deb1355777c4
dcouch440 <dcouch440@gmail.com>
2021-08-19 18:26:12 -0700

    change members to nested gyms

--- spec/requests/member/update_members_spec.rb
+++ spec/requests/member/update_members_spec.rb
@@ -4,6 +4,7 @@ describe 'Update Member', :type => :request do
 
   let!(:member) { FactoryBot.create(:member) }
   let!(:member_id) { member.id }
+  let!(:gym_id)  { member.gym_id }
   let!(:successful_patch_params) {{ name: "Terry", membership_active: 'inactive' }}
   let!(:unsuccessful_patch_params) {{ name: "Terry", membership_active: 'not active' }}
 
@@ -13,7 +14,7 @@ describe 'Update Member', :type => :request do
 
       it 'update a member' do
 
-        patch v1_member_path(member_id), params: successful_patch_params
+        patch "/v1/gyms/#{gym_id}/members/#{member_id}", params: successful_patch_params
 
         @response_name,
         @response_membership_active,
@@ -26,6 +27,7 @@ describe 'Update Member', :type => :request do
         expect(@response_name).to eql successful_patch_params[:name]
         expect(@response_membership_active).to eql successful_patch_params[:membership_active]
         expect(@response_id).to eql member_id
+        expect(response).to have_http_status :ok
 
       end
 
@@ -34,7 +36,7 @@ describe 'Update Member', :type => :request do
     context 'when unsuccessful' do
 
       it 'returns a status code of 422' do
-        patch v1_member_path(member_id), params: unsuccessful_patch_params
+        patch "/v1/gyms/#{gym_id}/members/#{member_id}", params: unsuccessful_patch_params
         expect(response).to have_http_status 422
       end
 
