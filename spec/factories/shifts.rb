9078223fd2a92a0b740181538ddd6155e44b1a74
dcouch440 <dcouch440@gmail.com>
2021-08-19 18:20:18 -0700

    change checkout_to nil

--- spec/factories/shifts.rb
+++ spec/factories/shifts.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :shift do
     check_in { "2021-08-17 16:04:13" }
-    check_out { "2021-08-17 16:04:13" }
+    check_out { nil }
     employee { build(:employee) }
   end
 end
