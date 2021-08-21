3af4e9b26df8031b9bd7a5de621292a1125e4b61
dcouch440 <dcouch440@gmail.com>
2021-08-21 13:01:13 -0700

    add index and update methods

--- app/controllers/v1/visits_controller.rb
+++ app/controllers/v1/visits_controller.rb
@@ -1,10 +1,31 @@
 class V1::VisitsController < ApplicationController
 
+  def index
+    json_response(Member.find(params[:member_id]).visits)
+  end
+
+  def update
+    visit = Member.find(params[:member_id]).visits.find(params[:id])
+    visit.update!(update_params)
+    json_response(visit)
+  end
+
+  def status
+    json_response(*Shift.add_visitation_status(visitation_params))
+  end
+
   def status
     json_response(*Visit.add_visitation_status(visitation_params))
   end
 
   private
+
+  def update_params
+    params.permit %i[
+      check_in
+      check_out
+    ]
+  end
 
   def visitation_params
     params.permit %i[
