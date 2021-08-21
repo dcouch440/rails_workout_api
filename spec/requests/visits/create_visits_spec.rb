e5bc86c189ee8d67ed946a365da7128440c54bb0
dcouch440 <dcouch440@gmail.com>
2021-08-21 13:02:01 -0700

    change test location

--- spec/requests/visits/create_visits_spec.rb
+++ spec/requests/visits/create_visits_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe 'CREATE Visit' do
 
-  describe '#create', :type => :request do
+  describe '#status check_in', :type => :request do
 
     let(:member) { FactoryBot.create(:member) }
     let!(:member_id) { member.id }
@@ -29,5 +29,33 @@ describe 'CREATE Visit' do
     end
 
   end
+
+  describe '#status - check_out', :type => :request do
+
+  let!(:visit) { FactoryBot.create(:visit) }
+  let!(:member_id) { visit.member_id }
+  let(:time) { '1111-11-01T08:00:00.000Z' }
+
+  context 'when successful' do
+
+    it 'lets a member check in' do
+      post v1_member_visits_path member_id, params: { time: time }
+      res = JSON.parse(response.body)
+      expect(res['check_out']).to eql time
+      expect(response).to have_http_status :ok
+    end
+
+  end
+
+  context 'when unsuccessful' do
+
+    it 'throws an error' do
+      post v1_member_visits_path member_id, params: { time: nil }
+      expect(response).to have_http_status :unprocessable_entity
+    end
+
+  end
+
+end
 
 end
