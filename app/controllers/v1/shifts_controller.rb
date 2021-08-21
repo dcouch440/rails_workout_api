5ba8baeef1ebf4dd7a3358d9b8ae03d91ab8f9fb
dcouch440 <dcouch440@gmail.com>
2021-08-21 13:01:06 -0700

    add index and update methods

--- app/controllers/v1/shifts_controller.rb
+++ app/controllers/v1/shifts_controller.rb
@@ -1,10 +1,27 @@
 class V1::ShiftsController < ApplicationController
 
+  def index
+    json_response(Employee.find(params[:employee_id]).shifts)
+  end
+
+  def update
+    shift = Employee.find(params[:employee_id]).shifts.find(params[:id])
+    shift.update!(update_params)
+    json_response(shift)
+  end
+
   def status
     json_response(*Shift.add_visitation_status(visitation_params))
   end
 
   private
+
+  def update_params
+    params.permit %i[
+      check_in
+      check_out
+    ]
+  end
 
   def visitation_params
     params.permit %i[
