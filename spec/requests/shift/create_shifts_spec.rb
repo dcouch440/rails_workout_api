0bad743a70553f54aab3009bce0c87e6b1524b9c
dcouch440 <dcouch440@gmail.com>
2021-08-21 13:01:55 -0700

    change test location

--- spec/requests/shift/create_shifts_spec.rb
+++ spec/requests/shift/create_shifts_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe 'CREATE shift' do
 
-  describe '#status', :type => :request do
+  describe '#status check_in', :type => :request do
 
     let(:employee) { FactoryBot.create(:employee) }
     let(:time) { '1111-11-01T08:00:00.000Z' }
@@ -28,5 +28,42 @@ describe 'CREATE shift' do
     end
 
   end
+
+  describe '#status check_out', :type => :request do
+
+  let(:employee) { FactoryBot.create(:employee) }
+  let(:employee_id) { employee.id }
+  let(:time) { '1111-11-01T08:00:00.000Z' }
+  let!(:shift) { employee.shifts.create!({ check_in: time }) }
+
+  context 'when successful' do
+
+    before do
+
+    end
+
+    it 'lets a employee check in' do
+      post v1_employee_shifts_path employee_id, params: {
+        time: time
+      }
+      res = JSON.parse(response.body)
+      expect(res['check_out']).to eql time
+      expect(response).to have_http_status :ok
+    end
+
+  end
+
+  context 'when unsuccessful' do
+
+    it 'throws an error' do
+      post v1_employee_shifts_path employee_id, params: {
+        time: nil
+      }
+      expect(response).to have_http_status :unprocessable_entity
+    end
+
+  end
+
+end
 
 end
